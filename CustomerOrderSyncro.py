ad98f333e78e0783e9f2996c84c4af1597aafa5c
gusya1 <serheos@bk.ru>
2021-11-06 00:41:02 +0300

    attribute wc_id added

--- CustomerOrderSyncro.py
+++ CustomerOrderSyncro.py
@@ -26,19 +26,6 @@ class CustomerOrderSyncro:
         self.customer_tag = customer_tag
         self.organization: Organization = list(MSApi.gen_organizations())[0]  # TODO choose organization
 
-        self.new_orders_state = None
-        self.paid_on_the_website = None
-        for state in CustomerOrder.gen_states():
-            if state.get_name() == "Новый":
-                self.new_orders_state = state
-            if state.get_name() == "оплачен на сайте":
-                self.paid_on_the_website = state
-
-        if self.new_orders_state is None:
-            raise RuntimeError("State \'new_orders_state\' not found")
-        if self.new_orders_state is None:
-            raise RuntimeError("State \'paid_on_the_website\' not found")
-
         self.states_dict = {}
         for state in CustomerOrder.gen_states():
             state: State
@@ -63,10 +50,17 @@ class CustomerOrderSyncro:
 
         for attr in Product.gen_attributes_list():
             if attr.get_name() == 'wc_id':
-                self.wc_id_href = attr.get_meta().get_href()
+                self.product_wc_id_href = attr.get_meta().get_href()
                 break
         else:
             raise RuntimeError("Product attribute \'{}\' not found".format('wc_id'))
+
+        for attribute in CustomerOrder.gen_attributes_list():
+            if attribute.get_name() == 'wc_id':
+                self.wc_id_attribute = attribute
+                break
+        else:
+            raise RuntimeError("CustomerOrder attribute \'{}\' not found".format('wc_id'))
 
         for order in MSApi.gen_customer_orders(limit=1, orders=Order.desc('created')):
             self.last_order_num = order.get_name()
@@ -74,75 +68,77 @@ class CustomerOrderSyncro:
 
     def sync_orders(self):
         for wc_order in WcApi.gen_all_wc(entity='orders', filters={'status': 'processing'}, cached=True):
-            ms_cp = self.__find_customer_order_by_phone(wc_order)
-            if ms_cp is None:
-                self.__find_customer_order_by_email(wc_order)
-            if ms_cp is None:
-                logging.debug("WC Order [{}]:\tCounterparty not found".format(wc_order.get('id')))
-                # ms_cp = self.__create_new_counterparty(wc_order)
-                continue
-
-            state = self.states_dict.get(wc_order.get('payment_method'))
-            if state is None:
-                logging.error("State for \'{}\' payment method not found")
-                continue
-
-            ms_post_order_data = {
-                'description': wc_order.get('customer_note'),
-                'externalCode': str(wc_order.get('id')),
-                'name': str(self.last_order_num + 1).zfill(5),
-                'organization': {'meta': self.organization.get_meta().get_json()},
-                'state': {'meta': state.get_meta().get_json()},
-                'agent': {'meta': ms_cp.get_meta().get_json()}
-            }
-
-            project = None
-            for meta_data in wc_order['meta_data']:
-                if meta_data['key'] == '_shipping_pickup_stores':
-                    project = self.projects_dict.get(meta_data['value'])
-            if project is not None:
-                ms_post_order_data['project'] = {'meta': project.get_meta().get_json()}
-
-            positions_post_data_list = []
-            is_successful = True
-            for wc_product in wc_order['line_items']:
-                wc_product_id = wc_product['product_id']
-                ms_product_list = list(MSApi.gen_products(filters=Filter.eq(self.wc_id_href, wc_product_id)))
-                if len(ms_product_list) == 0:
-                    logging.error("Product [{}] not found in MoySklad".format(wc_product_id))
-                    is_successful = False
-                    break
-                elif len(ms_product_list) != 1:
-                    logging.error("Product [{}] multiply definition in MoySklad".format(wc_product_id))
-                    is_successful = False
-                    break
-                ms_product = ms_product_list[0]
-                ms_post_position = {
-                    'assortment': {'meta': ms_product.get_meta().get_json()},
-                    'quantity': wc_product['quantity'],
-                    'price': wc_product['price'] * 100
-                }
-                positions_post_data_list.append(ms_post_position)
-            if not is_successful:
-                logging.info('Order not created')
-                continue
-            ms_post_order_data['positions'] = positions_post_data_list
+            wc_order_id = wc_order['id']
             try:
-                response = MSApi.auch_post("entity/customerorder", json=ms_post_order_data)
-                error_handler(response)
-                ms_order = CustomerOrder(response.json())
-                logging.info('CustomerOrder {} created'.format(ms_order.get_name()))
-                self.last_order_num += 1
-                wc_put_data = {
-                    'status': 'completed'
+                logging.info("WC Order [{}]:\tStarting syncro...".format(wc_order_id))
+                ms_cp = self.__find_customer_order_by_phone(wc_order)
+                if ms_cp is None:
+                    self.__find_customer_order_by_email(wc_order)
+                if ms_cp is None:
+                    logging.debug("WC Order [{}]:\tCounterparty not found".format(wc_order_id))
+                    ms_cp = self.__create_new_counterparty(wc_order)
+                if ms_cp is None:
+                    raise RuntimeError("Counterparty not found")
+                wc_payment_method = wc_order['payment_method']
+                state = self.states_dict.get(wc_payment_method)
+                if state is None:
+                    raise RuntimeError("State for \'{}\' payment method not found".format(wc_payment_method))
+
+                ms_post_order_data = {
+                    'description': wc_order.get('customer_note'),
+                    'externalCode': str(wc_order.get('id')),
+                    'name': str(self.last_order_num + 1).zfill(5),
+                    'organization': {'meta': self.organization.get_meta().get_json()},
+                    'state': {'meta': state.get_meta().get_json()},
+                    'agent': {'meta': ms_cp.get_meta().get_json()},
+                    'attributes': [
+                        {
+                            'meta': self.wc_id_attribute.get_meta().get_json(),
+                            'value': str(wc_order_id)
+                        }
+                    ]
                 }
-                WcApi.put('orders/{}'.format(wc_order['id']), data=wc_put_data)
-            except MSApiHttpException as e:
-                logging.error('CustomerOrder not created: {}'.format(str(e)))
-            except WcApiException as e:
-                logging.error('WC Order status not changed: {}'.format(str(e)))
 
+                project = None
+                for meta_data in wc_order['meta_data']:
+                    if meta_data['key'] == '_shipping_pickup_stores':
+                        project = self.projects_dict.get(meta_data['value'])
+                if project is not None:
+                    ms_post_order_data['project'] = {'meta': project.get_meta().get_json()}
 
+                positions_post_data_list = []
+                for wc_product in wc_order['line_items']:
+                    wc_product_id = wc_product['product_id']
+                    ms_product_list = list(MSApi.gen_products(filters=Filter.eq(self.product_wc_id_href, wc_product_id)))
+                    if len(ms_product_list) == 0:
+                        raise RuntimeError("Product [{}] not found in MoySklad".format(wc_product_id))
+                    elif len(ms_product_list) != 1:
+                        raise RuntimeError("Product [{}] multiply definition in MoySklad".format(wc_product_id))
+                    ms_product = ms_product_list[0]
+                    ms_post_position = {
+                        'assortment': {'meta': ms_product.get_meta().get_json()},
+                        'quantity': wc_product['quantity'],
+                        'price': wc_product['price'] * 100
+                    }
+                    positions_post_data_list.append(ms_post_position)
+
+                ms_post_order_data['positions'] = positions_post_data_list
+                try:
+                    response = MSApi.auch_post("entity/customerorder", json=ms_post_order_data)
+                    error_handler(response)
+                    ms_order = CustomerOrder(response.json())
+                    logging.info('CustomerOrder {} created'.format(ms_order.get_name()))
+                    self.last_order_num += 1
+                    wc_put_data = {
+                        'status': 'completed'
+                    }
+                    WcApi.put('orders/{}'.format(wc_order['id']), data=wc_put_data)
+                except MSApiHttpException as e:
+                    raise RuntimeError('CustomerOrder create failed: {}'.format(str(e)))
+                except WcApiException as e:
+                    logging.error('WC Order status change failed: {}'.format(str(e)))
+            except RuntimeError as e:
+                logging.error("WC Order [{}]: Synchronize failed: {}".format(wc_order_id, str(e)))
 
     @staticmethod
     def check_and_correct_ms_phone_numbers():
@@ -194,11 +190,10 @@ class CustomerOrderSyncro:
             elif len(ms_cp_list) > 1:
                 logging.warning("WC Order [{}]: Phone number \'{}\' multiply definition in MoySklad".format(
                     wc_order.get('id'), formatted_number))
-                return None
             else:
                 logging.debug("WC Order [{}]: Counterparty \'{}\' found by phone".format(wc_order.get('id'),
                                                                                          ms_cp_list[0].get_name()))
-                return ms_cp_list[0]
+            return ms_cp_list[0]
         except phonenumbers.phonenumberutil.NumberParseException:
             logging.warning("WC Order [{}]:\tInvalid phone \'{}\'".format(wc_order.get('id'), wc_phone_str))
             return None
@@ -216,7 +211,8 @@ class CustomerOrderSyncro:
             logging.debug("WC Order [{}]:\tCounterparty not found by email".format(wc_order.get('id')))
             return None
         elif len(ms_cp_list) > 1:
-            logging.warning("WC Order [{}]: Email \'{}\' multiply definition in MoySklad".format(wc_order.get('id'), wc_email_str))
+            logging.warning("WC Order [{}]: Email \'{}\' multiply definition in MoySklad".format(wc_order.get('id'),
+                                                                                                 wc_email_str))
             return None
         else:
             logging.debug("WC Order [{}]: Counterparty \'{}\' found by email".format(wc_order.get('id'),
